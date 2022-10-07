18ae25d64830fa04e9687bf23c91b642c23c047f
VitalyShpak9222 <112765255+VitalyShpak9222@users.noreply.github.com>
2022-10-07 23:31:14 +0300

    Klasswork completed

--- KW/Program.cs
+++ KW/Program.cs
@@ -8,155 +8,105 @@ namespace KW
 {
     class Program
     {
-        public static Dictionary<int, string> consoleDigits = new Dictionary<int, string>
+        static int maxchisl(int a, int b)
         {
-                [1] = " # \n" +
-                  "## \n" +
-                  " # \n" +
-                  " # \n" +
-                  "###",
+            return Math.Max(a, b);
+        }
 
-                [2] = " # \n" +
-                  "# #\n" +
-                  "  #\n" +
-                  " # \n" +
-                  "###",
+        static void mestznzch( ref int a ,ref int b)
+        {
+            int k = a;
+            a = b;
+            b = k;
+        }
 
-                [3] = "## \n" +
-                  "  #\n" +
-                  " # \n" +
-                  "  #\n" +
-                  "## ",
+        static bool factorial(int a)
+        {
+            try
+            {
+                int b = 1;
+                for (int i = 2; i < a; i++)
+                {
+                    b = checked((int)(b * i));
+                }
+                return true;
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
 
-                [4] = "# #\n" +
-                  "# #\n" +
-                  "###\n" +
-                  "  #\n" +
-                  "  #",
+        static uint Recurtions(uint a)
+        {
+            try
+            {
+                if (a == 1 || a == 0)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return a * Recurtions(a - 1);
+                }
+            }
+            catch(Exception error)
+            {
+                Console.WriteLine(error.Message);
+                return 0;
+            }
+        }
 
-                [5] = "###\n" +
-                  "#  \n" +
-                  "###\n" +
-                  "  #\n" +
-                  "## ",
-
-                [6] = " ##\n" +
-                  "#  \n" +
-                  "###\n" +
-                  "# #\n" +
-                  "###",
-
-                [7] = "###\n" +
-                  "  #\n" +
-                  "  #\n" +
-                  " # \n" +
-                  " # ",
-
-                [8] = "###\n" +
-                  "# #\n" +
-                  "###\n" +
-                  "# #\n" +
-                  "###",
-
-                [9] = "###\n" +
-                  "# #\n" +
-                  "###\n" +
-                  "  #\n" +
-                  "## ",
-
-                [0] = " # \n" +
-                  "# #\n" +
-                  "# #\n" +
-                  "# #\n" +
-                  " # "
-            };
+        static int Nod(int a, int b)
+        {
+            while (a != b)
+            {
+                b = Math.Max(a, b) - Math.Min(a, b);
+                a = Math.Min(a, b);
+            }
+            return a;
+        }
 
 
+        public static int Nod(int a, int b, int c)
+        {
+            int Nod = Math.Min(a, Math.Min(a, b));
+            for (; Nod > 1; Nod--)
+            {
+                if (a % Nod == 0 && b % Nod == 0 && c % Nod == 0)
+                    break;
+            }
+            return Nod;
+        }
+
+        static int Fap(int a)
+        {
+            if (a == 1)
+            {
+                return 1;
+            }
+            else if (a == 2)
+            {
+                return 1;
+            }
+            else if (a == 3)
+            {
+                return 2;
+            }
+            else if (a == 4)
+            {
+                return 3;
+            }
+            else
+            {
+                return (Fap(a - 1) + Fap(a - 2));
+            }
+        }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Задание 35\r\nПрограмма повторяет разговор Гарри Поттера из 2 части серии и меняет цвет консоли на один из 16 помимо черного");
-            Console.Clear();
-            Random color = new Random();
-            Console.ReadLine();
-            Console.WriteLine("Как тебя зовут?");
-            string name2 = Console.ReadLine();
-            Console.WriteLine("Привет, " + name2);
-            Console.ReadLine();
-            Console.WriteLine("Да");
-            Console.ReadLine();
-            Console.WriteLine("Нет");
-            System.Threading.Thread.Sleep(5000);
-            Console.WriteLine("Но могу показать");
-            int color1 = color.Next(1, 15);
-            if (color1 == 0)
-            {
-                Console.BackgroundColor = ConsoleColor.Black;
-            }
-            if (color1 == 1)
-            {
-                Console.BackgroundColor = ConsoleColor.DarkBlue;
-            }
-            if (color1 == 2)
-            {
-                Console.BackgroundColor = ConsoleColor.DarkGreen;
-            }
-            if (color1 == 3)
-            {
-                Console.BackgroundColor = ConsoleColor.DarkCyan;
-            }
-            if (color1 == 4)
-            {
-                Console.BackgroundColor = ConsoleColor.DarkRed;
-            }
-            if (color1 == 5)
-            {
-                Console.BackgroundColor = ConsoleColor.DarkMagenta;
-            }
-            if (color1 == 6)
-            {
-                Console.BackgroundColor = ConsoleColor.DarkYellow;
-            }
-            if (color1 == 7)
-            {
-                Console.BackgroundColor = ConsoleColor.Gray;
-            }
-            if (color1 == 8)
-            {
-                Console.BackgroundColor = ConsoleColor.DarkGray;
-            }
-            if (color1 == 9)
-            {
-                Console.BackgroundColor = ConsoleColor.Blue;
-            }
-            if (color1 == 10)
-            {
-                Console.BackgroundColor = ConsoleColor.Green;
-            }
-            if (color1 == 11)
-            {
-                Console.BackgroundColor = ConsoleColor.Cyan;
-            }
-            if (color1 == 12)
-            {
-                Console.BackgroundColor = ConsoleColor.Red;
-            }
-            if (color1 == 13)
-            {
-                Console.BackgroundColor = ConsoleColor.Magenta;
-            }
-            if (color1 == 14)
-            {
-                Console.BackgroundColor = ConsoleColor.Yellow;
-            }
-            if (color1 == 15)
-            {
-                Console.BackgroundColor = ConsoleColor.White;
-            }
-            Console.Clear();
-            System.Threading.Thread.Sleep(5000);
-            Console.ReadKey();
-            Console.ReadKey();
+
         }
     }
 }
